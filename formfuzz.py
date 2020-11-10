65edc57356997e8b3d04694b6418f9b6ad4709fc
Konrad Viskovic <kvisko444@gmail.com>
2020-11-10 01:36:08 +0100

    Update fuzzer

--- formfuzz.py
+++ formfuzz.py
@@ -11,6 +11,9 @@ from bs4 import BeautifulSoup
 import logging
 from utils import SuccessDetector
 
+# NEW
+from utils import Attack
+
 
 PARAMETER_PREFIX = "fuzz_"
 PREFIX_LEN = len(PARAMETER_PREFIX)
@@ -31,68 +34,21 @@ class FormFuzz:
 	# CLASS METHODS
 
 	def __init__(self):
-		#self.forms_list: list = []
-		self.fuzzdbs: dict = {"default":str(DBS_DIR + "default")}
-		self.fuzz_inputs: dict = {}
 		self.host_monitors: list = ["192.168.100.17"]
-		self.host_filter_string = ["~d"]						# TODO: Change to list
-		self.last_used_token = None
-		self.fresh_token = None
-		self.fuzzed_url = None
-		self.fuzz_flows = {"POST":None, "GET":None}
-		self.fuzz_in_progress = False
+		self.host_filter_string = ["~d"]
 
-		self.SD = SuccessDetector()
-		self.SD.setSuccessString("have logged in")
+		# NEW
+		self.attack = Attack()
 
-	# Handle detected form, returns dict 
-	def formHandler(self, flow: http.HTTPFlow) -> None:
-		to_fuzz = self.mapFuzzDict(flow.request)
-		if to_fuzz:
-			ctx.log.info("FormFuzz: fuzzing parameters were detected, starting fuzzing")
-			self.fuzz_flows["POST"] = flow.copy()
-			self.fuzzed_url = flow.request.url
-			self.loadFuzzInputs()
-			self.fuzz_in_progress = True
-
-
-	# Identify and map fuzzed parameters to fuzz files
-	def mapFuzzDict(self, parameters: net.http.request) -> bool:
+	def checkPOST(self, flow: http.HTTPFlow) -> bool:
 		fuzz = False
-		parameters = parameters.urlencoded_form
+		parameters = flow.request.urlencoded_form
 		for parameter in parameters:
 			value = parameters[parameter]
 			if value.startswith(PARAMETER_PREFIX):
-				self.fuzzdbs[parameter] = DBS_DIR + value[PREFIX_LEN:]
-				if not os.path.exists(self.fuzzdbs[parameter]):
-					ctx.log.error("FormFuzz: fuzz database " + self.fuzzdbs[parameter] + " does not exist")
-				else:
-					ctx.log.info("FormFuzz: fuzzing db choice " + str(parameter) + " => " + self.fuzzdbs[parameter])
-					fuzz = True
+				fuzz = True
+				break
 		return fuzz
-
-	def loadFuzzInputs(self) -> None:
-		for parameter in self.fuzzdbs:
-			inputs = self.loadFuzzParameters(parameter)
-			self.fuzz_inputs[parameter] = inputs
-			logger.info("LOADED FUZZ INPUTS FOR " + parameter)
-
-	# Load fuzz parameters from file into a dict
-	def loadFuzzParameters(self, parameter: str) -> list:
-		param_path = self.fuzzdbs[parameter]
-		if not os.path.exists(param_path):
-			ctx.log.info(param_path)
-			ctx.log.warn("FormFuzz: path " + param_path + " for parameter " + parameter + " does not exist")
-		with open(param_path, "r") as f:
-			fuzz_params = [x.strip() for x in f]
-		return fuzz_params
-
-	def extractCSRF(self, flow: http.HTTPFlow) -> str:
-		if not flow.response:
-			logger.info("ERROR: TRYING TO EXTRACT TOKEN FROM AN EMPTY RESPONSE")
-		parsed_html = BeautifulSoup(flow.response.content, features="html.parser")
-		csrf_token = parsed_html.body.find("input", attrs={"name":"user_token"}).get("value")
-		return csrf_token
 
 # MITM COMMANDS
 
@@ -111,70 +67,85 @@ class FormFuzz:
 	def load(self, entry: mitmproxy.addonmanager.Loader):
 		#print("FormFuzz addon loaded successfully.")
 		ctx.log.info("FormFuzz: addon loaded successfully")
-		ctx.master.commands.call("view.filter.set", "~d 192.168.100.17")
+		self.host_filter_string.append(self.host_monitors[0])
+		ctx.master.commands.call("view.filter.set", ' '.join(self.host_filter_string))
 
 	# Listen on requests and detect forms from specified hosts
 	def request(self, flow: http.HTTPFlow) -> None:
-		if flow.request.method == "POST" and flow.request.url == self.fuzzed_url:
-			self.last_used_token = flow.request.urlencoded_form["user_token"]
-			logger.info("SPENT CSRF TOKEN: " + self.last_used_token)
-		if flow.is_replay == "request":
-			return
-		if flow.request.method == "POST" and flow.request.host in self.host_monitors and not flow.is_replay == "request":
-			#ctx.log.info("FormFuzz: form data detected from " + str(flow.request.host))
-			self.formHandler(flow)
-		if flow.request.method == "GET" and flow.request.url == self.fuzzed_url:
-			logger.info("GOT GET REQUEST FROM CLIENT " + str(flow))
-			#ctx.master.commands.call("replay.client", [flow])
+		if self.attack.isRunning():
+			self.attack.handleRequest(flow)
+		else:
+			if flow.request.method == "POST" and flow.request.host in self.host_monitors and not flow.is_replay:
+				if self.checkPOST(flow):
+					self.attack.start(flow)
 
-		if flow.request.method == "GET":
-			return
 
-	
-	def response(self, flow: http.HTTPFlow):
-		if flow.request.method == "GET" and flow.request.host in self.host_monitors:
-			#logger.info("FormFuzz: RECEIVED NEW GET RESPONSE FROM SERVER: " + str(flow.response.text))
 
-			if self.fuzz_in_progress:
-				creds = self.SD.detector(flow)
-				if creds:
-					self.fuzz_in_progress = False
-					logger.info("Credentials: " + str(creds))
-					return
 
-				self.fresh_token = self.extractCSRF(flow)
-				#logger.info("GOT FRESH TOKEN: " + self.fresh_token)
-				self.fuzz_flows["GET"] = flow.copy()
+	#def request(self, flow: http.HTTPFlow) -> None:
+	#	if flow.request.method == "POST" and flow.request.url == self.fuzzed_url:
+	#		self.last_used_token = flow.request.urlencoded_form["user_token"]
+	#		logger.info("SPENT CSRF TOKEN: " + self.last_used_token)
+	#	if flow.is_replay == "request":
+	#		return
+	#	if flow.request.method == "POST" and flow.request.host in self.host_monitors and not flow.is_replay == "request":
+	#		#ctx.log.info("FormFuzz: form data detected from " + str(flow.request.host))
+	#		self.formHandler(flow)
+	#	if flow.request.method == "GET" and flow.request.url == self.fuzzed_url:
+	#		logger.info("GOT GET REQUEST FROM CLIENT " + str(flow))
+	#		#ctx.master.commands.call("replay.client", [flow])
 
-				post_flow = self.fuzz_flows["POST"].copy()
-				post_flow.request.urlencoded_form["user_token"] = self.fresh_token
-				for param in post_flow.request.urlencoded_form:
-					if param in self.fuzz_inputs:
-						if self.fuzz_inputs[param]:
-							post_flow.request.urlencoded_form[param] = self.fuzz_inputs[param].pop()
-							self.SD.setCredentials({param:post_flow.request.urlencoded_form[param]})
-							break
-						else:
-							logger.info("Fuzzing done, nothing found")
-							self.fuzz_in_progress = False
-							return
-				logger.info("FROM response REPLAYING POST REQUEST FORM WITH PARAMETERS " + str(post_flow.request.urlencoded_form))
-				ctx.master.commands.call("replay.client", [post_flow])
+	#	if flow.request.method == "GET":
+	#		return
 
-		if flow.request.method == "POST" and flow.request.url == self.fuzzed_url:
-			#logger.info("RECEIVED POST RESPONSE FROM SERVER " + str(flow.response.status_code))
-			self.fuzz_flows["POST"] = flow.copy()
-			if flow.response.status_code == 302 and self.fuzz_in_progress and self.fuzz_flows["GET"]:
-				get_flow = self.fuzz_flows["GET"].copy()
-				redirect_location = flow.response.headers["Location"]
-				logger.info("PATH COMPONENTS: " + str(get_flow.request.path_components))
-				get_flow.request.path_components = get_flow.request.path_components[:-1] + (redirect_location, )
-				logger.info("NEW PATH COMPONENTS:" + str(get_flow.request.path_components))
-				logger.info("NEW LOCATION IS: " + redirect_location)
-				logger.info("GET FLOW FROM 302: " + str(get_flow.request))
-				ctx.master.commands.call("replay.client", [get_flow])
-				#ctx.master.commands.call("replay.server", [flow])
-				logger.info("REPLAYED 302 TO SERVER")
+	def response(self, flow: http.HTTPFlow) -> None:
+		if self.attack.isRunning():
+			self.attack.handleResponse(flow)
+
+	#def response(self, flow: http.HTTPFlow):
+	#	if flow.request.method == "GET" and flow.request.host in self.host_monitors:
+	#		#logger.info("FormFuzz: RECEIVED NEW GET RESPONSE FROM SERVER: " + str(flow.response.text))
+
+	#		if self.fuzz_in_progress:
+	#			creds = self.SD.detector(flow)
+	#			if creds:
+	#				self.fuzz_in_progress = False
+	#				logger.info("Credentials: " + str(creds))
+	#				return
+
+		# 		self.fresh_token = self.extractCSRF(flow)
+		# 		#logger.info("GOT FRESH TOKEN: " + self.fresh_token)
+		# 		self.fuzz_flows["GET"] = flow.copy()
+
+		# 		post_flow = self.fuzz_flows["POST"].copy()
+		# 		post_flow.request.urlencoded_form["user_token"] = self.fresh_token
+		# 		for param in post_flow.request.urlencoded_form:
+		# 			if param in self.fuzz_inputs:
+		# 				if self.fuzz_inputs[param]:
+		# 					post_flow.request.urlencoded_form[param] = self.fuzz_inputs[param].pop()
+		# 					self.SD.setCredentials({param:post_flow.request.urlencoded_form[param]})
+		# 					break
+		# 				else:
+		# 					logger.info("Fuzzing done, nothing found")
+		# 					self.fuzz_in_progress = False
+		# 					return
+		# 		logger.info("FROM response REPLAYING POST REQUEST FORM WITH PARAMETERS " + str(post_flow.request.urlencoded_form))
+		# 		ctx.master.commands.call("replay.client", [post_flow])
+
+		# if flow.request.method == "POST" and flow.request.url == self.fuzzed_url:
+		# 	#logger.info("RECEIVED POST RESPONSE FROM SERVER " + str(flow.response.status_code))
+		# 	self.fuzz_flows["POST"] = flow.copy()
+		# 	if flow.response.status_code == 302 and self.fuzz_in_progress and self.fuzz_flows["GET"]:
+		# 		get_flow = self.fuzz_flows["GET"].copy()
+		# 		redirect_location = flow.response.headers["Location"]
+		# 		logger.info("PATH COMPONENTS: " + str(get_flow.request.path_components))
+		# 		get_flow.request.path_components = get_flow.request.path_components[:-1] + (redirect_location, )
+		# 		logger.info("NEW PATH COMPONENTS:" + str(get_flow.request.path_components))
+		# 		logger.info("NEW LOCATION IS: " + redirect_location)
+		# 		logger.info("GET FLOW FROM 302: " + str(get_flow.request))
+		# 		ctx.master.commands.call("replay.client", [get_flow])
+		# 		#ctx.master.commands.call("replay.server", [flow])
+		# 		logger.info("REPLAYED 302 TO SERVER")
 
 
 
--- utils.py
+++ utils.py
@@ -1,8 +1,16 @@
 import logging
 from mitmproxy import http
+from mitmproxy import net
+from mitmproxy import ctx
+import os.path
+from bs4 import BeautifulSoup
+
 
 logger = logging.getLogger("formfuzz")
 
+PARAMETER_PREFIX = "fuzz_"
+PREFIX_LEN = len(PARAMETER_PREFIX)
+DBS_DIR = "./dbs/"
 
 class SuccessDetector:
 
@@ -36,3 +44,149 @@ class SuccessDetector:
 			logger.info("GREAT SUCCESS! FOUND CORRECT CREDENTIALS: " + str(self.last_credentials))
 			result = self.last_credentials
 		return result
+
+
+
+class Attack:
+
+	def __init__(self):
+		self.running = False
+		self.fuzzdbs: dict = {}
+		self.fuzz_inputs: dict = {}
+		self.fuzzed_host = None
+		self.fuzzed_url = None
+		self.fuzz_flows = {"POST":None, "GET":None}
+		self.SD = None
+		self.originator_flow = None
+
+
+	def isRunning(self):
+		return self.running
+
+	def start(self, flow: http.HTTPFlow):
+		self.running = True
+		self.mapDBS(flow.request)
+		self.loadFuzzInputs()
+		self.fuzzed_url = flow.request.url
+		self.fuzzed_host = flow.request.host
+		self.originator_flow = flow.copy()
+		self.get_tmp = None
+
+		self.SD = SuccessDetector()
+		self.SD.setSuccessString("have logged in")
+
+	def stop(self):
+		self.running = False
+		self.fuzzdbs = {}
+		self.fuzz_inputs = {}
+		self.fuzzed_host = None
+		self.fuzzed_url = None
+		self.SD = None
+
+	# Identify and map fuzzed parameters to fuzz files
+	def mapDBS(self, parameters: net.http.request) -> None:
+		fuzz = False
+		parameters = parameters.urlencoded_form
+		for parameter in parameters:
+			value = parameters[parameter]
+			if value.startswith(PARAMETER_PREFIX):
+				self.fuzzdbs[parameter] = DBS_DIR + value[PREFIX_LEN:]
+				if not os.path.exists(self.fuzzdbs[parameter]):
+					logger.error("Fuzz database " + self.fuzzdbs[parameter] + " does not exist")
+				else:
+					logger.info("Loaded fuzz db path for parameter " + str(parameter) + " => " + self.fuzzdbs[parameter])
+
+	def loadFuzzInputs(self) -> None:
+		for parameter in self.fuzzdbs:
+			inputs = self.loadFuzzParameters(parameter)
+			self.fuzz_inputs[parameter] = inputs
+			logger.info("Loaded fuzz inputs for parameter: " + parameter)
+
+	# Load fuzz parameters from file into a dict
+	def loadFuzzParameters(self, parameter: str) -> list:
+		fuzz_params = []
+		param_path = self.fuzzdbs[parameter]
+		if not os.path.exists(param_path):
+			logger.warning("Path " + param_path + " for parameter " + parameter + " does not exist")
+		with open(param_path, "r") as f:
+			fuzz_params = [x.strip() for x in f]
+		return fuzz_params
+
+	def extractCSRF(self, flow: http.HTTPFlow) -> str:
+		if not flow.response:
+			logger.error("Trying to extract token from an empty response")
+		parsed_html = BeautifulSoup(flow.response.content, features="html.parser")
+		csrf_token = parsed_html.body.find("input", attrs={"name":"user_token"}).get("value")
+		return csrf_token
+
+	def prepareOriginatorReplay(self, token) -> http.HTTPFlow:
+		prepared_request_flow = self.originator_flow.copy()
+		prepared_request_flow.request.urlencoded_form["user_token"] = token
+		return prepared_request_flow
+
+	def setNextInput(self, flow: http.HTTPFlow) -> None:
+		for param in flow.request.urlencoded_form:
+			if param in self.fuzz_inputs:
+				if not self.fuzz_inputs[param]:
+					logger.warning("Fuzz input list is empty, terminating attack")
+					self.stop()
+					return
+
+				flow.request.urlencoded_form[param] = self.fuzz_inputs[param].pop()
+				self.SD.setCredentials({param:flow.request.urlencoded_form[param]})
+				break
+
+
+	def handleRequest(self, flow: http.HTTPFlow) -> None:
+		if flow.request.method == "POST" and flow.request.url == self.fuzzed_url:
+			logger.info("Spent CSRF token: " + flow.request.urlencoded_form["user_token"])
+		if flow.is_replay == "request":
+			#self.originator_flow = flow.copy()
+			#logger.info("Updated originator POST request flow")
+			return
+		if flow.request.method == "GET" and flow.request.url == self.fuzzed_url:
+			self.get_tmp = flow.copy()
+			logger.info("Updated tmp GET request flow")
+
+	def handleResponse(self, flow: http.HTTPFlow) -> None:
+		logger.info("Got response:" + str(flow.response))
+		if flow.request.method == "GET" and flow.request.host == self.fuzzed_host:
+			logger.info("Received response to GET from fuzzed host")
+
+			#if flow.request.url == self.fuzzed_url:
+			#	self.get_tmp = flow.request.copy()
+
+			credentials = self.SD.detector(flow)
+			if credentials:
+				self.stop()
+				logger.info("Found credentials, attack stopped")
+				logger.info("Credentials: " + str(credentials))
+				return
+
+			fresh_token = self.extractCSRF(flow)
+
+			prepared_request_flow = self.prepareOriginatorReplay(fresh_token)
+
+			# Not sure if copy() i needed in here
+			self.setNextInput(prepared_request_flow)
+			self.originator_flow = prepared_request_flow.copy()
+
+			logger.info("Replaying POST request form with parameters: " + str(prepared_request_flow.request.urlencoded_form))
+			ctx.master.commands.call("replay.client", [prepared_request_flow])
+
+		if flow.request.method == "POST" and flow.request.url == self.fuzzed_url:
+			logger.info("Before handling 302...")
+			if flow.response.status_code == 302:
+				if not flow.is_replay == "request":
+					logger.info("302 is not a response from replayed mitm request")
+					return
+				elif flow.is_replay == "request":
+					logger.info("POST request to this reply was a replay")
+					logger.info("GENERATING GET REPLAY")
+					get_flow = self.get_tmp.copy()
+					logger.info("get_flow request:" + str(get_flow.request))
+					redirect_location = flow.response.headers["Location"]
+					get_flow.request.path_components = get_flow.request.path_components[:-1] + (redirect_location, )
+					ctx.master.commands.call("replay.client", [get_flow])
+					#ctx.master.commands.call("replay.server", [flow])
+					#logger.info("REPLAYED 302 TO SERVER")
